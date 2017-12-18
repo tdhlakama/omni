f19a5b75eac3e60c4b7cb7b789bb7641b0a31c3a
tdhlakama <tdhlakama@gmail.com>
2017-12-18 19:18:02 +0200

    serve response added to system

---
+++ app/src/main/java/zw/co/tk/omnichannel/model/ServerResponse.java
@@ -0,0 +1,23 @@
+package zw.co.tk.omnichannel.model;
+
+public class ServerResponse {
+
+    private String status;
+    private Long id;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
